2773244482b5a9d0b4370666b49e461a0c33eccb
Reei-dp <slimsslims1337@gmail.com>
2026-07-28 01:25:54 +0400

    Update widget tests for new app structure.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,12 +10,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:asteriaray/l10n/app_localizations.dart';
 import 'package:asteriaray/notifiers/app_settings_notifier.dart';
 import 'package:asteriaray/notifiers/profile_notifier.dart';
 import 'package:asteriaray/notifiers/vpn_notifier.dart';
 import 'package:asteriaray/screens/home_screen.dart';
+import 'package:asteriaray/notifiers/routing_notifier.dart';
+import 'package:asteriaray/models/routing_profile.dart';
 import 'package:asteriaray/models/vless_profile.dart';
+import 'package:asteriaray/services/routing_profile_store.dart';
+import 'package:asteriaray/notifiers/subscription_notifier.dart';
 import 'package:asteriaray/services/profile_store.dart';
+import 'package:asteriaray/services/subscription_store.dart';
 import 'package:asteriaray/services/xray_runner.dart';
 import 'package:asteriaray/services/vpn_platform.dart';
 
@@ -27,6 +33,7 @@ class _FakeRunner extends XrayRunnerBase {
   Future<XrayConfigContext> prepareConfig(
     VlessProfile profile, {
     bool useDoh = false,
+    RoutingProfile? routing,
   }) async {
     return XrayConfigContext(
       configPath: '/tmp/config.json',
@@ -36,7 +43,11 @@ class _FakeRunner extends XrayRunnerBase {
   }
 
   @override
-  Map<String, dynamic> buildConfig(VlessProfile profile, bool useDoh) => {};
+  Map<String, dynamic> buildConfig(
+    VlessProfile profile,
+    bool useDoh, {
+    RoutingProfile? routing,
+  }) => {};
 }
 
 class _FakePlatform extends VpnPlatform {
@@ -54,6 +65,7 @@ class _FakePlatform extends VpnPlatform {
     String? profileName,
     String? transport,
     String? vlessServerHost,
+    String? localeCode,
   }) async {}
 
   @override
@@ -61,6 +73,7 @@ class _FakePlatform extends VpnPlatform {
     required String conf,
     required String profileName,
     String? profileId,
+    String? localeCode,
   }) async {}
 
   @override
@@ -78,6 +91,14 @@ class _FakePlatform extends VpnPlatform {
   @override
   Future<Map<String, int>> getStats() async =>
       {'upload': 0, 'download': 0};
+
+  @override
+  Future<int?> measureVlessDelay({
+    required String configJson,
+    required String configPath,
+    required String workDir,
+    String testUrl = 'https://www.google.com/generate_204',
+  }) async => null;
 }
 
 void main() {
@@ -90,23 +111,44 @@ void main() {
     final appSettings = await AppSettingsNotifier.create();
     final runner = _FakeRunner();
     final platform = _FakePlatform();
+    final subStore = await SubscriptionStore.create();
+    final routingStore = await RoutingProfileStore.create();
+    final routingNotifier = RoutingNotifier(routingStore);
+    await routingNotifier.init();
+    final subscriptionNotifier = SubscriptionNotifier(
+      subStore,
+      profileNotifier,
+      runner: runner,
+      platform: platform,
+      appSettings: appSettings,
+      routing: routingNotifier,
+    );
+    await subscriptionNotifier.init();
 
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           ChangeNotifierProvider.value(value: profileNotifier),
+          ChangeNotifierProvider.value(value: subscriptionNotifier),
+          ChangeNotifierProvider.value(value: routingNotifier),
           ChangeNotifierProvider.value(value: appSettings),
           ChangeNotifierProvider(
             create: (_) => VpnNotifier(
               runner,
               platform: platform,
               appSettings: appSettings,
+              routing: routingNotifier,
             ),
           ),
         ],
-        child: const MaterialApp(home: HomeScreen()),
+        child: MaterialApp(
+          localizationsDelegates: const [AppLocalizations.delegate],
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: const HomeScreen(),
+        ),
       ),
     );
+    await tester.pumpAndSettle();
 
     expect(find.text('Asteria 🚀'), findsOneWidget);
   });
